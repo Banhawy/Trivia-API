88d085b6241854b7b8a39e93bcc9181e094bb4f9
Adham Army <the.benhawy@gmail.com>
2020-11-07 02:25:06 +0200

    added env variables

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 from flaskr import create_app
 from models import setup_db, Question, Category
-
+import config
 
 class TriviaTestCase(unittest.TestCase):
     """This class represents the trivia test case"""
@@ -14,8 +14,7 @@ class TriviaTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = config.test_database_path
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
