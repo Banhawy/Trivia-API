85d8ccbd26947844b2ac0af567e3ca139b7b66e6
Adham Army <the.benhawy@gmail.com>
2020-11-09 22:20:19 +0200

    added delete API endpoint

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -83,12 +83,29 @@ def create_app(test_config=None):
       'categories': categories
     })
   '''
-  @TODO: 
+  @DONE: 
   Create an endpoint to DELETE question using a question ID. 
 
   TEST: When you click the trash icon next to a question, the question will be removed.
   This removal will persist in the database and when you refresh the page. 
   '''
+  @app.route('/questions/<int:question_id>', methods=['DELETE'])
+  def delete_question(question_id):
+    try:
+      question = Question.query.filter(Question.id == question_id).one_or_none()
+
+      if question is None:
+        abort(404)
+
+      question.delete()
+
+      return jsonify({
+        'success': True,
+        'deleted': question_id
+      })
+
+    except:
+      abort(422)
 
   '''
   @DONE: 
