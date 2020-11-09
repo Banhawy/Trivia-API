34c4b096ab7e74627a1bbf90498508c97e43acfc
Adham Army <the.benhawy@gmail.com>
2020-11-09 22:19:35 +0200

    added delete test case

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -91,6 +91,14 @@ class TriviaTestCase(unittest.TestCase):
         data = json.loads(res.data)
         
         self.assertEqual(res.status_code, 200)
+    
+    def test_delete_question_failed(self):
+        res = self.client().delete('/questions/name')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
